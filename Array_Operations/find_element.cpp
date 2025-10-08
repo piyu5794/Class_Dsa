2ed914abcb29946ad583e367a39990b12e5d7286
piyu5794 <piyu5794@gmail.com>
2025-10-08 11:51:08 +0530

    Update find_element.cpp

--- Array_Operations/find_element.cpp
+++ Array_Operations/find_element.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter size of an array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"enetr elements of array :";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+        // cout<<endl;
+    }
 
+    int x;
+    cout<<"enter an element :";
+    cin>>x;
+    for(int i=0; i<n; i++){
+        if(arr[i]==x){
+            cout<<"element is present "<<endl;
+            break;
+        }
+        cout<<"element is finding"<<endl;
+
+        // if(arr[n-1]!=x){
+        //     cout<<"element is not present ";
+        // }
+        if(i==n-1 && arr[i]!=x){
+            cout<<"element is not present ";
+        }
+    }
+
+    return 0;
+} 

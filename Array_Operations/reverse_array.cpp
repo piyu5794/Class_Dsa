c4d3f0ddb6116cc43c5d4b634941c3ad81967fcd
piyu5794 <piyu5794@gmail.com>
2025-10-08 11:53:14 +0530

    Update reverse_array.cpp

--- Array_Operations/reverse_array.cpp
+++ Array_Operations/reverse_array.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+#include<vector>
 
+using namespace std;
+
+void display(vector<int>&v){
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+int main()
+{
+    vector<int> v{1,3,2,4,3,4,1,6};
+    display(v);
+
+        //swap two vectors to rverse
+        for(int i=0, j=v.size()-1; j>=i;i++, j--){
+            int temp =v[i];
+            v[i]=v[j];
+            v[j]=temp;
+        
+    }
+        //creting a new vector to reverse
+    // vector<int>v1(v.size());
+    // for(int i=0; i<v.size(); i++){
+    //     v1[i]=v[v.size()-1-i];
+    // }
+    
+    display(v);
+
+    return 0;
+}
